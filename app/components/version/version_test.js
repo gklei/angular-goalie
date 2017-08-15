0dc7c332ab0ee84a0c543b986ddd5341490910cb
gklei <gregory@incipia.co>
2017-08-15 12:32:22 -0400

    Initial commit.

--- app/components/version/version_test.js
+++ app/components/version/version_test.js
@@ -1,7 +1,7 @@
 'use strict';
 
-describe('myApp.version module', function() {
-  beforeEach(module('myApp.version'));
+describe('goalie.version module', function() {
+  beforeEach(module('goalie.version'));
 
   describe('version service', function() {
     it('should return current version', inject(function(version) {
--- app/view2/view2_test.js
+++ app/view2/view2_test.js
@@ -1,8 +1,8 @@
 'use strict';
 
-describe('myApp.view2 module', function() {
+describe('goalie.view2 module', function() {
 
-  beforeEach(module('myApp.view2'));
+  beforeEach(module('goalie.view2'));
 
   describe('view2 controller', function(){
 
